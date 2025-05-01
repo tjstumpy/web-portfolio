f3e03a798371ef1fb04edbc31a9461d9dbb4cc8f
Tim Stumper <tjstumpy@udel.edu>
2025-05-01 14:13:51 -0400

    fixing netlify build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { webcrypto } from 'crypto';
 
-// https://vite.dev/config/
+if (!globalThis.crypto) globalThis.crypto = webcrypto;
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
